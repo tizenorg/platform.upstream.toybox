45511501f855ebccf4c495f63f2f96417c245613
Jan Cybulski <j.cybulski@samsung.com>
2014-10-22 14:47:23 +0200

    mkdir: add -Z option
    Change-Id: I14ad41aa327df30b917947671ed88fd06e94ebd0
    Signed-off-by: Jan Cybulski <j.cybulski@samsung.com>

--- toys/posix/mkdir.c
+++ toys/posix/mkdir.c
@@ -4,19 +4,20 @@
  *
  * See http://opengroup.org/onlinepubs/9699919799/utilities/mkdir.html
 
-USE_MKDIR(NEWTOY(mkdir, "<1vpm:", TOYFLAG_BIN|TOYFLAG_UMASK))
+USE_MKDIR(NEWTOY(mkdir, "<1vpm:Z:", TOYFLAG_BIN|TOYFLAG_UMASK))
 
 config MKDIR
   bool "mkdir"
   default y
   help
-    usage: mkdir [-vp] [-m mode] [dirname...]
+    usage: mkdir [-vp] [-m mode] [-Z context] [dirname...]
 
     Create one or more directories.
 
     -m	set permissions of directory to mode.
     -p	make parent directories as needed.
     -v	verbose
+    -Z	security context
 */
 
 #define FOR_mkdir
@@ -27,6 +28,7 @@ config MKDIR
 #endif //USE_SMACK
 
 GLOBALS(
+  char *arg_context;
   char *arg_mode;
 )
 
@@ -35,6 +37,10 @@ void mkdir_main(void)
   char **s;
   mode_t mode = (0777&~toys.old_umask);
 
+#ifdef USE_SMACK
+    if( smack_set_label_for_self(TT.arg_context) < 0)
+      error_exit("Unable to create directory with '%s' as context.\n", TT.arg_context);
+#endif
 
   if (TT.arg_mode) mode = string_to_mode(TT.arg_mode, 0777);
 
